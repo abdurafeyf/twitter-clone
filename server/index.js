6e58ca7e6b8c534d03b74570ad3ebff25cd1bc3d
abdurafeyf <92419613+abdurafeyf@users.noreply.github.com>
2023-11-24 09:16:18 +0500

    installed cors

--- server/index.js
+++ server/index.js
@@ -31,6 +31,7 @@ app.use(cors({ origin: "https://twitter-backend-tawny.vercel.app" }));
 
 app.use(cookieParser());
 app.use(express.json());
+
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/tweets", tweetRoutes);
